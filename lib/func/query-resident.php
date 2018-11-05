85a1d0fc72e82c140bcfc50e664d4406486d793f
Cole <cole@thresholdcomputing.net>
2018-11-04 20:58:13 -0600

    Added a function that can take inputs to return the row ID, this will be usefull for looking up users, or when looking up links to other tables

--- lib/func/query-resident.php
+++ lib/func/query-resident.php
@@ -22,3 +22,25 @@ function listResidents() {
     displayResult($result, $sql);
 
 }
+
+/**
+ * @param $tableName - The table with the record being searched for
+ * @param $idField - the ID Field, (Usually {Table-Name}_ID)
+ * @param $fieldName - The field you are searching by
+ * @param $searchTerm -  What you are searching for
+ *
+ * @return mysqli_result
+ */
+function GetRowValue ($tableName, $idField, $fieldName, $searchTerm) {
+	databaseConnection("damascus_way");
+	$conn = new mysqli(DBF_SERVER, DBF_USER, DBF_PASSWORD, DBF_NAME);
+	if($conn->connect_error){
+		die("Connection Failed! ". mysqli_connect_error());
+	}
+
+	$sql = "SELECT " . $idField . " FROM " . $tableName . " WHERE " . $fieldName . " LIKE " . $searchTerm;
+
+	$result = $conn->query($sql);
+
+	return $result;
+}
