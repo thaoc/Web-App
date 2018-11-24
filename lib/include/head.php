ebbe04b65c0de21c4f984655cf51f543e0475357
xrhodia <xiongr2@csp.edu>
2018-11-24 07:14:35 -0600

    Added in stylesheet for search button

--- lib/include/head.php
+++ lib/include/head.php
@@ -8,6 +8,7 @@
 	<meta name="author" content="Damascus Way">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 
     <?php
         echo "<link rel='icon' type='image/png' href='" . $homeDir . "/img/favicon.png'>";
