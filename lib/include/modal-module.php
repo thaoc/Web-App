a34f6cdc6191c174cc0a16a30203af706bdb4594
Cole <cole@thresholdcomputing.net>
2018-11-24 10:37:07 -0600

    Started writing the success listener

---
+++ lib/include/modal-module.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project Name: WebProject.
+ * File Name: modal-module.php.
+ * User: Cole
+ * Date: 11/24/2018
+ * Time: 10:26 AM
+ */
+
+// Trigger/Open The Modal
+echo "<button id='successBtn'></button>";
+
+// The Modal
+echo "<div id='successModal' class='modal'>";
+
+// Modal content
+echo "<div class='modal-content'>";
+echo "<span class='close'>&times;</span>";
+echo "<p>You successfully added the record</p>";
+echo "</div>";
+echo "</div>";
