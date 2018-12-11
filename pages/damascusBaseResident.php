3d79118eb319c268ece74ac1633e64cf7e1252ee
Cole <cole@thresholdcomputing.net>
2018-12-11 09:40:36 -0600

    Added form control classes to the form to make it responsive, also added the checkin classes to maintain style continuity

--- pages/damascusBaseResident.php
+++ pages/damascusBaseResident.php
@@ -23,34 +23,34 @@
             <h2>Resident Intake</h2>
          </div>
          <div class="row dw-row">
-            <!--<label>Offender ID:<span class="req"></span></label></br>
-               <input type="text" name="offenderID" id="offenderInput" value="Offender ID"/></br></br>-->
+            <!--<label class="form-control highlight">Offender ID:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderID" id="offenderInput" value="Offender ID"/></br></br>-->
             <div class="col-md-4">
-               <label>First Name:<span class="req"></span></label></br>
-               <input type="text" name="offenderFirstName" id="offenderInput" value="" tabindex="1"/></br></br>
+               <label class="form-control highlight">First Name:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderFirstName" id="offenderInput" value="" tabindex="1"/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Middle Name:<span class="req"></span></label></br>
-               <input type="text" name="offenderMiddleName" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Middle Name:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderMiddleName" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Last Name:<span class="req"></span></label></br>
-               <input type="text" name="offenderLastName" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Last Name:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderLastName" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Photo:<span class="req"></span></label></br>
-               <input type="file" name="offenderImage" id="offenderInput"></br></br>
+               <label class="form-control highlight">Photo:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="file" name="offenderImage" id="offenderInput"></br></br>
             </div>
             <div class="col-md-4">
-               <label>Birthplace:<span class="req"></span></label></br>
-               <input type="text" name="offenderBirthplace" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Birthplace:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderBirthplace" id="offenderInput" value=""/></br></br>
             </div>
          </div>
          <!-- End Row 1 -->
          <div class="row">
             <div class="col-md-4">
-               <label for="offenderOffense">Offense:<span class="req"></span></label></br>
-               <select name="offenderOffense" id="offenderInput">
+               <label class="form-control highlight" for="offenderOffense">Offense:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderOffense" id="offenderInput">
                   <?PHP
                      // Loop through the runner table to build the <option> list
                      $sql = "SELECT Offense_ID, Offense_Type AS 'offense'
@@ -65,16 +65,16 @@
 			  </br></br>
             </div>
             <div class="col-md-4">
-               <label>Sex Offender?<span class="req"></span></label></br>
-               <select name="sexOffender" id="offenderInput">
+               <label class="form-control highlight">Sex Offender?<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="sexOffender" id="offenderInput">
                   <option value="1">Yes</option>
                   <option value="0">No</option>
                </select>
                <br><br>
             </div>
             <div class="col-md-4">
-               <label for="offenderRiskLevel">Risk Level:<span class="req"></span></label></br>
-               <select name="offenderRiskLevel" id="offenderInput">
+               <label class="form-control highlight" for="offenderRiskLevel">Risk Level:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderRiskLevel" id="offenderInput">
                   <?PHP
                      // Loop through the runner table to build the <option> list
                      $sql = "SELECT Risk_Level_ID, Risk_Level_Type AS 'riskLevel'
@@ -92,16 +92,16 @@
          <!-- End Row 2 -->
          <div class = "row dw-row">
             <div class="col-md-4">
-               <label>Admit Date:<span class="req"></span></label></br>
-               <input type="date" name="offenderAdmitDate" id="offenderInput"/></br></br>
+               <label class="form-control highlight">Admit Date:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="date" name="offenderAdmitDate" id="offenderInput"/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Exit Date:<span class="req"></span></label></br>
-               <input type="date" name="offenderExitDate" id="offenderInput"/></br></br>
+               <label class="form-control highlight">Exit Date:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="date" name="offenderExitDate" id="offenderInput"/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Facility:<span class="req"></span></label></br>
-               <select name="offenderFacility" id="offenderInput">
+               <label class="form-control highlight">Facility:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderFacility" id="offenderInput">
                   <option value="1">Rochester</option>
                   <option value="2">Golden Valley</option>
                </select>
@@ -114,23 +114,23 @@
          <br>
          <div class="row dw-row">
             <div class="col-md-4">
-               <label>Room #:<span class="req"></span></label></br>
-               <input type="text" name="offenderRoomNumber" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Room #:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderRoomNumber" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Caseworker<span class="req"></span></label></br>
-               <input type="text" name="offenderCaseworker" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Caseworker<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderCaseworker" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>CW Phone #:<span class="req"></span></label></br>
-               <input type="tel" name="offenderCWPhoneNumber" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">CW Phone #:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="tel" name="offenderCWPhoneNumber" id="offenderInput" value=""/></br></br>
             </div>
          </div>
          <!-- End Row 4 -->
          <div class="row dw-row">
             <div class="col-md-4">
-               <label for="offenderEyeColor">Eye Color:<span class="req"></span></label></br>
-               <select name="offenderEyeColor" id="offenderInput">
+               <label class="form-control highlight" for="offenderEyeColor">Eye Color:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderEyeColor" id="offenderInput">
                   <?PHP
                      // Loop through the runner table to build the <option> list
                      $sql = "SELECT Eye_Color_ID, Eye_Color_Description AS 'eyeColor'
@@ -145,8 +145,8 @@
                </br></br>
             </div>
             <div class="col-md-4">
-               <label for="offenderHairColor">Hair Color:<span class="req"></span></label></br>
-               <select name="offenderHairColor" id="offenderInput">
+               <label class="form-control highlight" for="offenderHairColor">Hair Color:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderHairColor" id="offenderInput">
                   <?PHP
                      // Loop through the runner table to build the <option> list
                      $sql = "SELECT Hair_Color_ID, Hair_Color_Description AS 'hairColor'
@@ -161,27 +161,27 @@
 			   </br></br>
             </div>
             <div class="col-md-4">
-               <label>Height(inches):<span class="req"></span></label></br>
-               <input type="number" name="offenderHeight" value=""/></br></br>
+               <label class="form-control highlight">Height(inches):<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="number" name="offenderHeight" value=""/></br></br>
             </div>
          </div>
          <!-- End Row 5 -->
          <div class="row dw-row">
             <div class="col-md-4">
-               <label>Weight:<span class="req"></span></label></br>
-               <input type="number" name="offenderWeight" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Weight:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="number" name="offenderWeight" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Sex:<span class="req"></span></label></br>
-               <select name="offenderSex"/>
+               <label class="form-control highlight">Sex:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderSex"/>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                </select>
                </br></br>
             </div>
             <div class="col-md-4">
-               <label for="offenderRace">Race:<span class="req"></span></label></br>
-               <select name="offenderRace" id="offenderInput">
+               <label class="form-control highlight" for="offenderRace">Race:<span class="req"></span></label></br>
+               <select class="form-control checkin"  name="offenderRace" id="offenderInput">
                   <?PHP
                      // Loop through the runner table to build the <option> list
                      $sql = "SELECT Race_ID, Race_Description AS 'race'
@@ -196,41 +196,41 @@
 			   </br></br>
             </div>
             <div class="col-md-4">
-               <label>Date of Birth:<span class="req"></span></label></br>
-               <input type="date" name="offenderDateOfBirth" id="offenderInput" placeholder="YYYY-MM-DD"/></br></br>
+               <label class="form-control highlight">Date of Birth:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="date" name="offenderDateOfBirth" id="offenderInput" placeholder="YYYY-MM-DD"/></br></br>
             </div>
          </div>
          <!-- End Row 6 -->
          <div class ="row dw-row">
             <div class="col-md-4">
-               <label>Agent Name:<span class="req"></span></label></br>
-               <input type="text" name="offenderAgent" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Agent Name:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderAgent" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Agent Phone #:<span class="req"></span></label></br>
-               <input type="tel" name="offengerAgentPhoneNumber" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Agent Phone #:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="tel" name="offengerAgentPhoneNumber" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Agent Address:<span class="req"></span></label></br>
-               <input type="text" name="offenderAgentAddress" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Agent Address:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="text" name="offenderAgentAddress" id="offenderInput" value=""/></br></br>
             </div>
          </div>
          <!-- End Row 7 -->
          <div class="row dw-row">
             <div class="col-md-4">
-               <label>Username:<span class="req"></span></label></br>
-               <input type="username" name="offenderUsername" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Username:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="username" name="offenderUsername" id="offenderInput" value=""/></br></br>
             </div>
             <div class="col-md-4">
-               <label>Password:<span class="req"></span></label></br>
-               <input type="password" name="offenderPassword" id="offenderInput" value=""/></br></br>
+               <label class="form-control highlight">Password:<span class="req"></span></label></br>
+               <input class="form-control checkin"  type="password" name="offenderPassword" id="offenderInput" value=""/></br></br>
             </div>
          </div>
          <!-- End Row 8 -->
          <div class="row dw-row">
             <div class="col-md-12">
                <div class="submit">
-                  <input type="submit" value="Submit" class="button">
+                  <input class="form-control checkin"  type="submit" value="Submit" class="button">
                </div>
             </div>
          </div>
